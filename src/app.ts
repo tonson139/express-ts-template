02171e9bafa496a27a2c9f545b330a576bb3da27
patanindev <tonson.pa@gmail.com>
2021-08-21 22:25:53 +0700

    typerom connected

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,26 @@ app.get("/ping", async (_req, res) => {
 	});
 });
 
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Photo } from "./entity/Photo";
+import { User } from "./entity/User";
+
+createConnection()
+	.then((connection) => {
+		let newPhoto = new Photo();
+
+		newPhoto.name = "Me and Bears";
+		newPhoto.description = "I am near polar bears";
+		newPhoto.filename = "photo-with-bears.jpg";
+		newPhoto.views = 1;
+		newPhoto.isPublished = true;
+		console.log(newPhoto);
+
+		return connection.manager.save(newPhoto).then((newPhoto) => {
+			console.log("Photo has been saved. Photo id is", newPhoto.id);
+		});
+	})
+	.catch((error) => console.log(error));
+
 export default app;
