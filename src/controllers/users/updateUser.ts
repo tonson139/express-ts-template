bd156b79ff235590928c4104bbea44c00f9f8d01
patanindev <tonson.pa@gmail.com>
2021-08-29 12:50:31 +0700

    add updateUserbyId

--- src/controllers/users/updateUser.ts
+++ src/controllers/users/updateUser.ts
@@ -1,12 +1,24 @@
 import type { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../../models/entity/User";
+import { responseHandler } from "../../helpers/responseHandler";
 
 export const updateUser = async (
 	req: Request,
 	res: Response
 ): Promise<Response> => {
-	console.log("updateUser");
-	const mockdata = `This is updateUser id: ${req.params.id}`;
-	return res.status(200).json({
-		data: mockdata,
-	});
+	const userRepository = getRepository(User);
+
+	const { id } = req.params;
+	const updatedUser = req.body;
+
+	await userRepository.update(id, updatedUser);
+	const [users, count] = await userRepository.findByIds([id]);
+
+	let responseData = {
+		data: [users],
+		count: [users].length,
+	};
+
+	return responseHandler(res, responseData, 200);
 };
